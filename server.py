10a4896bf99bc43907b067c91233e2cfac6d5c08
seki <schokolade0317@gmail.com>
2020-07-08 16:58:19 +0900

    make server

---
+++ server.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, make_response, jsonify
+import os
+import werkzeug
+from datetime import datetime
+
+from keras.models import model_from_json
+from keras.preprocessing import image
+import numpy as np
+
+# flask
+app = Flask(__name__)
+
+# ★ポイント1
+# limit upload file size : 1MB
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
+
+# ★ポイント2
+# ex) set UPLOAD_DIR_PATH=C:/tmp/flaskUploadDir
+UPLOAD_DIR = "upload"
+
+
+# rest api : request.files with multipart/form-data
+# <form action="/data/upload" method="post" enctype="multipart/form-data">
+#   <input type="file" name="uploadFile"/>
+#   <input type="submit" value="submit"/>
+# </form>
+@app.route('//upload', methods=['POST'])
+def upload_multipart():
+    # ★ポイント3
+    if 'uploadFile' not in request.files:
+        make_response(jsonify({'result': 'uploadFile is required.'}))
+
+    file = request.files['uploadFile']
+    fileName = file.filename
+    if '' == fileName:
+        make_response(jsonify({'result': 'filename must not empty.'}))
+
+    # ★ポイント4
+    saveFileName = datetime.now().strftime("%Y%m%d_%H%M%S_") \
+                   + werkzeug.utils.secure_filename(fileName)
+    file.save(os.path.join(UPLOAD_DIR, saveFileName))
+
+    print(saveFileName)
+
+    # 保存したモデルの読み込み
+    model = model_from_json(open('model/stomach.json').read())
+    # 保存した重みの読み込み
+    model.load_weights('model/stomach.hdf5')
+
+    categories = ["broken", "fat"]
+
+    # 画像を読み込む
+    img_path = "upload/" + saveFileName
+    img = image.load_img(img_path, target_size=(250, 250, 3))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    # 予測
+    features = model.predict(x)
+    return make_response(jsonify({'result': {"broken": str(features[0, 0]), "notBroken": str(features[0, 1])}}))
+
+
+# ★ポイント5
+@app.errorhandler(werkzeug.exceptions.RequestEntityTooLarge)
+def handle_over_max_file_size(error):
+    print("werkzeug.exceptions.RequestEntityTooLarge")
+    return 'result : file size is overed.'
+
+
+# main
+if __name__ == "__main__":
+    from waitress import serve
+
+    serve(app, host='127.0.0.1', port=3000)
